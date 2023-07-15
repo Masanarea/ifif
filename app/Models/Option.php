1ad357fd83552ff6a9a1cf58da9c40f3f508f230
aki <aki@akis-MacBook-Air.local>
2023-07-15 10:30:22 +0900

    不足分のマイグレーションファイルとモデルを作成

---
+++ app/Models/Option.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    // タイムスタンプの保存に使用するカラム名をカスタマイズ
+    const CREATED_AT = "ins_timestamp";
+    const UPDATED_AT = "upd_timestamp";
+}
